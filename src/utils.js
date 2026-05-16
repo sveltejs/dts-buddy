719982c175d131f73095c2e35d0dd498dccc78fe
Shun Git <kwokshungit@gmail.com>
2026-05-16 14:20:44 +0800

    handle PropertyDeclaration

--- src/utils.js
+++ src/utils.js
@@ -410,7 +410,7 @@ export function get_dts(file, created, resolve, options) {
 				current = previous;
 			} else {
 				walk(node, (node) => {
-					if (ts.isPropertySignature(node) && is_internal(node) && options.stripInternal) {
+					if (options.stripInternal && is_internal(node) && is_property(node)) {
 						return false;
 					}
 
@@ -596,6 +596,13 @@ export function is_reference(node, include_declarations = false) {
 	}
 
 	return true;
+}
+
+/** @param {ts.Node} node */
+export function is_property(node) {
+	if (ts.isPropertySignature(node)) return true;
+	if (ts.isPropertyDeclaration(node)) return true;
+	return false;
 }
 
 /**
